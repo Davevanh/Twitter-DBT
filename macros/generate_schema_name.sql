0bc57797f48e63c2d3a372aa3b94a5bf8bb0361d
Davevanh <davevheukelom@gmail.com>
2019-08-08 19:50:29 +0100

    Add files via upload

---
+++ macros/generate_schema_name.sql
@@ -0,0 +1,16 @@
+-- Overwrite the default macro that changes handles custom schemas
+
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{ custom_schema_name }}
+
+    {%- endif -%}
+
+{%- endmacro %}
